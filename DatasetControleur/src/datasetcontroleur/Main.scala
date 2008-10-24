e834675661d192e4f949722a4195f21b9cac9620
mdoboi <ghostinlimbo@gmail.com>
2008-10-24 22:54:51 +0200

    nettoyage

--- DatasetControleur/src/datasetcontroleur/Main.scala
+++ DatasetControleur/src/datasetcontroleur/Main.scala
@@ -22,8 +22,10 @@ trait DbTemplate {
 //implémentations
 trait SpringJdbcTemplate extends DbTemplate {
   var dbTemplate:JdbcTemplate = null
-  def execute(query:String) = {println(query)
-                               dbTemplate execute query}
+  def execute(query:String) = {
+    println(query)
+    dbTemplate execute query
+  }
   def queryForList(query:String, params:Array[Object]):JavaList[_] =  dbTemplate.queryForList(query,params)
   def queryForList(query:String, params:Array[Object], returnType:Class[_]):JavaList[_] =  dbTemplate.queryForList(query,params,returnType)
 }
@@ -39,7 +41,7 @@ trait Db2TableIntrospectionComponent extends TableIntrospectionComponent{ this: 
   }
 }
 
-class DatasetControleur requires (DatasetControleur with DbTemplate with TableIntrospectionComponent) extends DatasetCommandComponent {
+class DatasetControleur { this: DbTemplate with TableIntrospectionComponent with DatasetCommandComponent =>
   val datasetCommand = new DatasetCommand
   val queryBuilder = new QueryBuilder
 }
@@ -51,37 +53,37 @@ object Main {
    */
   def main(args: Array[String]) = {
     val context = new ClassPathXmlApplicationContext(Array[String]("config/config.xml"))
-    val jdbcTemplate = (context getBean "DummyDao").asInstanceOf[DummyDao].getJdbcTemplate
+    val jdbcTemplate = (context getBean "DummyDao").asInstanceOf[DummyDao] getJdbcTemplate
     val txManager = (context getBean "txManager").asInstanceOf[DataSourceTransactionManager]
     val txDef = new DefaultTransactionDefinition
     txDef.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW)
-
-    val action = args(0)
     
     val file = this.getClass().getClassLoader getResource "config/proprietes" getFile
 
-    //lecture du fichier de configuration et récupération de la ligne définissant le dataset
-    val lines = for {
-      (line) <- Source fromFile(file) getLines
-    } yield (line)
-    val line = lines.find({x:String => x.indexOf("dataset") != -1})
-    val fileName = line.getOrElse(throw new Exception("dataset file name not found")).split("=")(1)
+    //lecture du fichier de configuration et récupération de la ligne définissant le dataset si le fichier n'est pas donné en argument
+    val fileName = if (args.length < 2){
+      (Source fromFile(file) getLines)
+      .find {(_:String) startsWith "dataset="}
+      .getOrElse(throw new Exception("dataset file name not found")).split("=")(1)
+    } else args(1)
     
-    val dataset = XML loadFile(fileName)
-    val controleur = new DatasetControleur with SpringJdbcTemplate with Db2TableIntrospectionComponent
+    
+    val dataset = XML loadFile fileName
+    val controleur = new DatasetControleur with SpringJdbcTemplate with Db2TableIntrospectionComponent with DatasetCommandComponent
     controleur.dbTemplate = jdbcTemplate
 
+    val action = args(0)
     if ("testInsert" == action){
-      val txStatus = txManager getTransaction(txDef)
+      val txStatus = txManager getTransaction txDef
       try{
         controleur.datasetCommand insert dataset
-      } finally{
-        txManager rollback(txStatus)
+      } finally {
+        txManager rollback txStatus
       }
     }else if("delete" == action){
       controleur.datasetCommand delete dataset
     }else if("insert" ==action){
-        controleur.datasetCommand insert dataset
+      controleur.datasetCommand insert dataset
     }
     print("cool ('"+action+"' lines from "+ fileName +")")
   }
--- DatasetControleur/src/datasetcontroleur/QueryBuilder.scala
+++ DatasetControleur/src/datasetcontroleur/QueryBuilder.scala
@@ -7,25 +7,25 @@ import TableIntrospection.TypeParColonneType
 class QueryBuilder {
 
   def buildInsertQuery(tableName:String, attributes:Iterable[(_,_)], typesColonne:TypeParColonneType):String = {
-    return "INSERT INTO "+tableName+"(" + ((attributes.elements.map {x:(_,_) => x._1}).mkString(", ")
-                                        + ") VALUES ("
-                                        + (attributes.elements.map {x:(_,_) => format(x._2, x._1.toString, typesColonne)}).mkString(", ") + ")")
+    "INSERT INTO "+tableName+"(" + ((attributes.elements.map {(_:(_,_))._1}).mkString(", ")
+                                 + ") VALUES ("
+                                 + (attributes.elements.map {x:(_,_) => format(x._2, x._1.toString, typesColonne)}).mkString(", ") + ")")
   }
 
   def buildDeleteQuery(tableName:String, attributes:Iterable[(_,_)], colonnesPks:List[String], typesColonne:TypeParColonneType):String = {
     // Récupération des attributs XML à garder dans la clause where du delete, c.a.d ceux correspondant aux colonnes formant la pk de la table
-    val pkAttributes = attributes filter {x:(_,_) => (colonnesPks find {c:String => c == x._1}) isDefined}
-    return "DELETE FROM "+tableName+" WHERE " + ((pkAttributes.elements.map {x:(_,_) => x._1 +" = "+ format(x._2, x._1.toString, typesColonne)}) mkString " AND ")
+    val pkAttributes = attributes filter {colonnesPks contains (_:(_,_))._1}
+    "DELETE FROM "+tableName+" WHERE " + ((pkAttributes.elements.map {x:(_,_) => x._1 +" = "+ format(x._2, x._1.toString, typesColonne)}) mkString " AND ")
   }
 
   /**
    * formatte la valeur en fonction du type de la colonne
    */
   private def format[A,B,C](paramValeur:A, cleColonne:B, typesColonne:HashMap[B, C]):String = {
+    def quote = "'"+ StringEscapeUtils.escapeSql(_:String) +"'"
     val valeur = paramValeur.toString
-    def quote(v:String):String = "'"+ StringEscapeUtils.escapeSql(v) +"'"
     val typeColonne = typesColonne get cleColonne getOrElse (throw new Exception("!! Colonne non trouvée pour la clé [" + cleColonne + "] !!"))
-    return typeColonne match{
+    typeColonne match{
       case "SMALLINT" | "BIGINT" | "INTEGER" | "DECIMAL" => valeur
       case "BLOB" => "BLOB(" + quote(valeur) + ")"
       case "TIMESTAMP" => quote(if (valeur.size < 11){ valeur + " 00:00:00.0" }else{valeur})
